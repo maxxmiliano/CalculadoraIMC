dd7fbcc992d947114854ba36bef2967d1133d115
unknown <maxmilianossouza@hotmail.com>
2023-03-17 01:21:48 -0300

    atualizando projeto

---
+++ src/cal.js
@@ -0,0 +1,7 @@
+const Digite_Nome =document.getElementById('nome');
+const Sua_altura =document.getElementById('altura');
+const Seu_peso =document.getElementById('peso');
+const Calcular =document.getElementById('calcular');
+const Resultado =document.getElementById('resultado');
+
+
