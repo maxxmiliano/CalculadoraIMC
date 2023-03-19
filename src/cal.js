09d94d8c994d307d3e99d563a4f88dd5d7fed462
unknown <maxmilianossouza@hotmail.com>
2023-03-19 20:50:12 -0300

    projeto finalizado

--- src/cal.js
+++ src/cal.js
@@ -15,13 +15,25 @@ function imc () {
 
         let classificacao = '';
         if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.'
+            classificacao = 'Abaixo do peso.';
         }
-        else if (valorIMC){
-            classificacao =
+        else if (valorIMC < 25) {
+            classificacao = 'Com peso ideal. Parabéns!!!';
+        }
+        else if (valorIMC < 30) {
+            classificacao = 'Um pouco acima do peso.';
+        }
+        else if (valorIMC < 35 ) {
+            classificacao = 'Com obesidade grau 1.';
+        }
+        else if ( valorIMC < 40 ) {
+            classificacao = 'Com obesidade grau 2.';
+        }
+        else  {
+            classificacao = 'Com odesidade grau 3 cuidado.';
         }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e voce esta ${classificacao}`;
+          resultado.textContent = `${nome} seu IMC é ${valorIMC} e voce esta ${classificacao}`;
         
     }
      else {
